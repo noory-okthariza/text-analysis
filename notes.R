cf79f87efb16f7132cee9f9f7afbd8c52dca4164
Your Name <your-email@example.comgit>
2024-12-06 18:14:33 -0500

    commit

---
+++ notes.R
@@ -0,0 +1,6 @@
+#Note
+wordcloud and wordcloud2 is two different thing
+wordcloud2 is more advanced and fit for interactive output
+but it doesnt automatically remove stopwords, or other words that are not 
+making sense, but wordcloud can do better
+
