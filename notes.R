b199193f1c2d463cce5a9a9567b2b397be69c540
Your Name <your-email@example.comgit>
2024-12-08 12:04:58 -0500

    commit

--- notes.R
+++ notes.R
@@ -4,3 +4,10 @@ wordcloud2 is more advanced and fit for interactive output
 but it doesnt automatically remove stopwords, or other words that are not 
 making sense, but wordcloud can do better
 
+wordcloud alone can handle overlap words much better than wordcloud2
+
+
+# it's not really about findings or empirical
+but it's about what i learn what i experience from using this text data techniqe
+first, worldcloud difference
+fine-tuning it's also important
